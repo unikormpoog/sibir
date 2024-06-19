68ed1d4a166ae0cde36f02370aacba8a3ff7b2a1
1 <btio6303@gmail.com>
2024-06-19 21:31:01 +0300

    пиздец блять 1

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -24,10 +24,13 @@ export default function SideNav() {
         //     await signOut();
         //   }}
         >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <Link
+            href="/"
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          >
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Выход</div>
-          </button>
+          </Link>
         </form>
       </div>
     </div>
